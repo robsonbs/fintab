daf76ae9322413be2c984ab71f14e70c1682d0b3
robson.souza <robson.souza@presidencia.gov.br>
2026-03-27 18:04:36 -0300

    feat: add Home component with gratitude messages

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+function Home() {
+  return (
+    <>
+      <h1>Agradecimentos</h1>
+      <p>
+        Obrigado <strong>pai</strong>, por todos os ensinamentos de vida e
+        profissionais, se estou onde estou foi devido as experiências que tem
+        compartilhado ao longo de todo meu desenvolvimento!
+      </p>
+      <p>
+        Obrigado <strong>mãe</strong>, pela vida e todo cuidado dedicado aos
+        meus passos durante esses anos.
+      </p>
+      <p>
+        Obrigado <strong>irmã</strong>, por sempre me incentivar a nunca
+        desistir dos sonhos.
+      </p>
+    </>
+  );
+}
+
+export default Home;
