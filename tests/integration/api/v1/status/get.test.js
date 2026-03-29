27afe34b3460c0ea097568b0e90da379fcbce82a
Robson Barbosa Souza <powerrob92@gmail.com>
2026-03-29 14:38:45 -0300

    feat: implement database query functionality and update status API endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,11 +1,10 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3002/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
 
-
 test("GET to /api/v1/status should return status ok", async () => {
-  const response = await fetch("http://localhost:3002/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");
   const data = await response.json();
 
   expect(response.status).toBe(200);
