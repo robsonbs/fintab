203960d0b5c4bae8077e822e46cc25197046ae46
Robson Barbosa Souza <powerrob92@gmail.com>
2026-03-29 12:48:20 -0300

    feat: add status API endpoint and corresponding integration tests

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,13 @@
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3002/api/v1/status");
+  expect(response.status).toBe(200);
+});
+
+
+test("GET to /api/v1/status should return status ok", async () => {
+  const response = await fetch("http://localhost:3002/api/v1/status");
+  const data = await response.json();
+
+  expect(response.status).toBe(200);
+  expect(data).toEqual({ status: "ok" });
+});
