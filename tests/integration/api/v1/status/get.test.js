f891c0252599afe3c95d4c8ee35cba629c0b7231
Robson Barbosa Souza <powerrob92@gmail.com>
2026-03-30 09:42:42 -0300

    feat: enhance status API to return database connection details and updated timestamp

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -5,8 +5,19 @@ test("GET to /api/v1/status should return 200", async () => {
 
 test("GET to /api/v1/status should return status ok", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  const data = await response.json();
+  const responseBody = await response.json();
 
   expect(response.status).toBe(200);
-  expect(data).toEqual({ status: "ok" });
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
+  expect(responseBody.dependencies.database.open_connections).toBeDefined();
+  expect(responseBody.dependencies.database.open_connections).toEqual(1);
 });
