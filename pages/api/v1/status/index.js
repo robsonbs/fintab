eed176de192c8e384fc7bd91cfb67314a14dcfcc
robson.souza <robson.souza@presidencia.gov.br>
2026-03-30 09:42:42 -0300

    feat: enhance status API to return database connection details and updated timestamp

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query({
+    text: `SELECT (SELECT COUNT(*)::int from pg_stat_activity WHERE datname = $1) as "openConnections", current_setting('max_connections')::int as "maxConnections", current_setting('server_version') as "serverVersion"`,
+    values: [process.env.POSTGRES_DB],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult.rows[0].serverVersion,
+        max_connections: databaseVersionResult.rows[0].maxConnections,
+        open_connections: databaseVersionResult.rows[0].openConnections,
+      },
+    },
+  });
 }
 
 export default status;
