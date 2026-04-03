a30f7d247d977120c6fc62adc427e81d362fd2fb
Robson Barbosa Souza <powerrob92@gmail.com>
2026-04-03 17:25:35 -0300

    fix: correct import statement for migration runner

--- infra/migration.js
+++ infra/migration.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import migrationRunner from "node-pg-migrate";
+import { runner as migrationRunner } from "node-pg-migrate";
 import { join } from "node:path";
 
 const MIGRATIONS_DIR = join("infra", "migrations");
