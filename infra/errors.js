4ea4428e87096919f93e753d960f40704c9e92c0
Robson Barbosa Souza <powerrob92@gmail.com>
2026-04-04 08:30:06 -0300

    feat: add MethodNotAllowedError class for improved error handling

--- infra/errors.js
+++ infra/errors.js
@@ -16,3 +16,21 @@ export class InternalServerError extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP utilizado é suportado por este endpoint e tente novamente.";
+    this.statusCode = 405;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
